de14e47b652bbb7a93060e4deaecfa07c18ad06d
scott-howe-1 <scott.d.howe@seagate.com>
2023-04-07 12:12:46 -0500

    add minimal testing capability to make sure the result queue (queue_out) is working correctly.

--- example.c
+++ example.c
@@ -9,47 +9,82 @@
  * decide which thread will run what. So it is not an error of the thread pool but rather
  * a decision of the OS.
  *
+ * CLI compile:
+ * gcc example.c thpool.c -D THPOOL_DEBUG -pthread -o example
  * */
 
 #include <stdio.h>
 #include <pthread.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "thpool.h"
 
 int task(void *arg){
 
-	int result = (int)(intptr_t)arg  + 10;
+	int result = (int)(intptr_t)arg  + 100;
 
-	printf("Thread #%u: parm:%d, ret:%d\n",
+	printf("Thread #%u: uuid:%d, ret:%d\n",
 	       (int)pthread_self(), (int)(intptr_t)arg, result);
 
 	return result;
 }
 
+#define NUM_LOOPS 100
+#define NUM_TASKS 100
+
+void gen_random_numbers(int array[], int len, int min, int max){
+	int i;
+	for (i = 0; i < len; i++)
+		array[i] = rand() % (max - min + 1) + min;
+}
+
+void gen_numbers(int array[], int len){
+	int i;
+	for (i = 0; i < len; i++)
+		array[i] = i;
+}
 
 int main(){
+	int arr_uuid[NUM_TASKS];
+	int num_errs=0;
 
-	puts("Making threadpool with 4 threads");
-	threadpool thpool = thpool_init(4);
+	srand( time(NULL) );
 
-	puts("\nAdding 40 tasks to threadpool");
-	int i;
-	int result;
-	for (i=0; i<40; i++){
-		thpool_add_work(thpool, i, task, (void*)(uintptr_t)i);
-	};
+	gen_random_numbers(arr_uuid, NUM_TASKS, 1, NUM_TASKS);
+	// gen_numbers(arr_uuid, NUM_TASKS);
 
-	thpool_wait(thpool);
+	printf("Main Thread #%u\n", (int)pthread_self());
+	int j;
+	for (j=0; j<NUM_LOOPS; j++){
+		puts("Making threadpool with 4 threads");
+		threadpool thpool = thpool_init(4);
 
-	puts("\nRetreiving 40 task results");
-	for (i=0; i<40; i++){
-		thpool_get_result(thpool, i, &result);
-		printf("uuid:%d, result:%d\n", i, result);
-	};
+		puts("\nAdding tasks to threadpool");
+		int i;
+		int result;
+		for (i=0; i<NUM_TASKS; i++){
+			thpool_add_work(thpool, arr_uuid[i], task, (void*)(uintptr_t)arr_uuid[i]);
+		};
 
-	thpool_wait(thpool);
-	puts("\nKilling threadpool");
-	thpool_destroy(thpool);
+		// thpool_wait(thpool);
+
+		puts("\nRetreiving results from threadpool");
+		for (i=0; i<NUM_TASKS; i++){
+			thpool_get_result(thpool, arr_uuid[i], &result);
+			printf("main: success uuid:%d, result:%d\n", arr_uuid[i], result);
+			if (result != arr_uuid[i]+100)
+				num_errs++;
+		};
+
+		thpool_wait(thpool);
+		puts("\nKilling threadpool");
+		thpool_destroy(thpool);
+
+		printf("main: completed loop %d\n", j);
+	}
+	printf("main: num_errs = %d\n", num_errs);
 
 	return 0;
 }

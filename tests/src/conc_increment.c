a52789c8f7cb0d5cc717be034fa3a42d05b30e5f
scott-howe-1 <scott.d.howe@seagate.com>
2023-04-12 17:38:18 -0500

    add ability to test using the thpool_get_result() api to do the waiting (with large timeouts)

--- tests/src/conc_increment.c
+++ tests/src/conc_increment.c
@@ -22,13 +22,14 @@ int increment() {
 int main(int argc, char *argv[]){
 
 	char* p;
-	if (argc != 4){
-		puts("This testfile needs exactly 3 arguments");
+	if (argc != 5){
+		puts("This testfile needs exactly 4 arguments");
 		exit(1);
 	}
 	int num_jobs    = strtol(argv[1], &p, 10);
 	int num_threads = strtol(argv[2], &p, 10);
 	int use_results = strtol(argv[3], &p, 10);
+	int use_wait    = strtol(argv[4], &p, 10);
 
 	threadpool thpool = thpool_init(num_threads);
 
@@ -37,13 +38,16 @@ int main(int argc, char *argv[]){
 		thpool_add_work(thpool, n, (void*)increment, NULL);
 	}
 
-	thpool_wait(thpool);
+	if (use_wait)
+		thpool_wait(thpool);
 
 	if (use_results){
 		int result;
 		int sum_results=0;
+		int ret;
 		for (n=0; n<num_jobs; n++){
-			thpool_get_result(thpool, n, 10000, 10000, &result);
+			ret = thpool_get_result(thpool, n, 10000, 10000, &result);
+			printf("ret=%d\n", ret);
 			sum_results += result;
 		}
 
--- tests/threadpool.sh
+++ tests/threadpool.sh
@@ -8,10 +8,10 @@
 . funcs.sh
 
 
-function test_mass_addition { #endsum #threads #useresults
-	echo "Adding up to $1 with $2 threads with result flag $3"
+function test_mass_addition { #endsum #threads #useresults #use_wait
+	echo "Adding up to $1 with $2 threads (flags: result $3 wait $4)"
 	compile src/conc_increment.c
-	output=$(./test $1 $2 $3)
+	output=$(./test $1 $2 $3 $4)
 	sum_threads=$(echo $output | awk '{print $(NF)}')
 	if [ "$sum_threads" != "$1" ]; then
 		err "Expected $1 from threads but got $output" "$output"
@@ -28,11 +28,18 @@ function test_mass_addition { #endsum #threads #useresults
 
 
 # Run tests
-test_mass_addition 100 4 0
-test_mass_addition 100 1000 0
-test_mass_addition 100000 1000 0
-test_mass_addition 100 4 1
-test_mass_addition 100 1000 1
-test_mass_addition 100000 1000 1
+
+# use_results = True, use_wait = True
+test_mass_addition 100 4 1 1
+test_mass_addition 100 1000 1 1
+test_mass_addition 100000 1000 1 1
+# use_results = True, use_wait = False
+test_mass_addition 100 4 1 0
+test_mass_addition 100 1000 1 0
+test_mass_addition 100000 1000 1 0
+# use_results = False, use_wait = True
+test_mass_addition 100 4 0 1
+test_mass_addition 100 1000 0 1
+test_mass_addition 100000 1000 0 1
 
 echo "No errors"
